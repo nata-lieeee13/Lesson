e7967dc78f68296efcfe3b18fc1e75b17c74d5c0
nata-lieeee13 <730484@student.inholland.nl>
2025-02-14 13:03:44 +0100

    Changed Divide Method
    /by 0 not allowed

--- projesct/projesct/Program.cs
+++ projesct/projesct/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace projesct
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace projesct
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        public void Divide(double a, double b)
+        {
+            if(b ==0)
+            {
+                Console.WriteLine("Ërror:Division by zero is not AllowedValuesAttribute");
+            }
+            else
+            {
+                Console.WriteLine(a / b);
+            }
         }
     }
 }
